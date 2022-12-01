d93f90d9518d2c49dda3dfe7799060848e14b017
SP1N4X <82057630+SP1N4X@users.noreply.github.com>
2022-12-01 11:34:16 +0000

    modifiche

--- .github/workflows/testscript.py
+++ .github/workflows/testscript.py
@@ -13,10 +13,12 @@ def CalcoloTabSpecificaCasaTras(tableGeneraleCasa, tableGeneraleTras):
 
 def CalcoloGoalFattiSubiti(tableSpecifica):
     trs = tableSpecifica.find_all('tr')
-    goalHTFatti = trs[15]
-    goalHTSubiti =
-    goal2HTFatti =
-    goal2HTSubiti =
+
+    goalHTFatti = trs[14].find('b').text
+    goalHTSubiti = trs[15].find('b').text
+    goal2HTFatti = trs[17].find('b').text
+    goal2HTSubiti = trs[18].find('b').text
+
     return goalHTFatti, goalHTSubiti, goal2HTFatti, goal2HTSubiti
 
 def CalcoloTable(table):
@@ -24,10 +26,14 @@ def CalcoloTable(table):
     tableSpecificaCasa, tableSpecificaTras = CalcoloTabSpecificaCasaTras(tableGeneraleCasa, tableGeneraleTras) 
     goalHTFattiCasa, goalHTSubitiCasa, goal2HTFattiCasa, goal2HTSubitiCasa = CalcoloGoalFattiSubiti(tableSpecificaCasa)
     goalHTFattiTras, goalHTSubitiTras, goal2HTFattiTras, goal2HTSubitiTras = CalcoloGoalFattiSubiti(tableSpecificaTras)
+    return goalHTFattiCasa, goalHTSubitiCasa, goal2HTFattiCasa, goal2HTSubitiCasa, goalHTFattiTras, goalHTSubitiTras, goal2HTFattiTras, goal2HTSubitiTras
 
+def CalcoloGoalFromHtml(html):
+    for table in html.find_all('table'):
+        if table.find('font', text = 'GOAL PER TIMES SEGMENT') != null:
+            gHTFattiCasa, gHTSubitiCasa, g2HTFattiCasa, g2HTSubitiCasa, gHTFattiTras, gHTSubitiTras, g2HTFattiTras, g2HTSubitiTras = CalcoloTable(table)
+            
+            print(gHTFattiCasa, gHTSubitiCasa, g2HTFattiCasa, g2HTSubitiCasa, gHTFattiTras, gHTSubitiTras, g2HTFattiTras, g2HTSubitiTras)
+            break
 
-for table in html.find_all('table'):
-    if table.find('font', text = 'GOAL PER TIMES SEGMENT') != null:
-        CalcoloTable(table)
-        print('fatto')
-        break
+CalcoloGoalFromHtml(html)
