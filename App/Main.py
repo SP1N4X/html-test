92ba9c1b31832e71af80ba3c3125d353682b4ba9
SP1N4X <futplayer.fp@gmail.com>
2022-12-04 17:21:51 +0100

    modifiche

--- App/Main.py
+++ App/Main.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from Funzioni import FindMatch
 
-day = 3
+day = input()
 n_partite = 10
 differenza = 0.3
 
@@ -10,9 +10,13 @@ url = "https://www.soccerstats.com/matches.asp?matchday=" + str(day) + "&listing
 url_base = "https://www.soccerstats.com/"
 
 response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
-rows = soup.find("table", {"id": "btable"}).find("tbody").find_all("tr")
-for row in rows:
-    risultato = FindMatch(row, url_base, n_partite, differenza)
-    if risultato != '':
-        print(risultato)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, 'html.parser')
+    rows = soup.find("table", {"id": "btable"}).find("tbody").find_all("tr")
+    for row in rows:
+        risultato = FindMatch(row, url_base, n_partite, differenza)
+        if risultato != '':
+            print(risultato)
+else:          
+    print('Errore nella risposta riprovare') 
