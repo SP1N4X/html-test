c549a9267cbd9f05af4d09a1e2749e958aceec36
SP1N4X <82057630+SP1N4X@users.noreply.github.com>
2022-12-05 11:32:25 +0000

    Add files via upload

---
+++ ScrapeCampionato.py
@@ -0,0 +1,6 @@
+def TrovaCampionato(soup):
+    try:
+        campionato = soup.find("div", {"id": "content"}).find_all('div')[0].find_all('div')[1].find('font').text
+        return campionato
+    except:
+        return 'Campionato non trovato'
