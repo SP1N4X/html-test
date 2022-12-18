744ee2944d953e3175662d621e1e20721985150e
SP1N4X <futplayer.fp@gmail.com>
2022-12-18 13:36:23 +0100

    modic

--- test.py
+++ test.py
@@ -1,7 +1,26 @@
+def intTry(value):
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
+def downloadMatch(casa, tras):
+    print(casa, tras)
+
+def CheckMatch(line):
+    split = line.split('-')
+    casa = split[0]
+    tras = split[1]
+    if intTry(casa) or intTry(tras):
+        return 
+    downloadMatch(casa, tras)
+
 today = '18-12-2022'
 with open(f'{today}.txt', 'r') as f:
     lines = f.readlines()
     i = 0
     while i < len(lines):
-        print(lines[i])
+        line = lines[i]
+        CheckMatch(line)
         i += 1
