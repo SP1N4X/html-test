d24217741894670a6d309a66c5764104b398222e
SP1N4X <82057630+SP1N4X@users.noreply.github.com>
2022-12-18 12:27:05 +0100

    Delete ScrapeCampionato.py

--- ScrapeCampionato.py
+++
@@ -1,6 +0,0 @@
-def TrovaCampionato(soup):
-    try:
-        campionato = soup.find("div", {"id": "content"}).find_all('div')[1].find_all('div')[1].find('font').text
-        return campionato
-    except:
-        return 'Campionato non trovato'
