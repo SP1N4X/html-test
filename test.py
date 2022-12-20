be0a7bba526e06f40ab37697ad89432e5dbf6286
SP1N4X <futplayer.fp@gmail.com>
2022-12-20 16:03:18 +0100

    modif

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+json =  {}
+oggetto = {}
 def intTry(value):
     try:
         int(value)
@@ -14,13 +16,91 @@ def CheckMatch(line):
     tras = split[1]
     if intTry(casa) or intTry(tras):
         return 
-    downloadMatch(casa, tras)
+    oggetto['casa'] = casa
+    oggetto['trasferta'] = tras
 
-today = '18-12-2022'
+def CheckCampionato(line):
+    line.replace('(', '')
+    line.replace(')', '')
+    oggetto['campionato'] = line
+
+def PrevisioneTipo(line):
+    line.replace(']', '')
+    line.replace('[', '')
+    return line
+
+def CheckEsatti(line):
+    oggetto['risultato'] = line
+
+def CheckCorner(line):
+    split = line.split(' ')
+    oggetto['Corner'] = split[1]
+
+def CheckMin(line):
+    split = line.split(':')
+    if split[0] == 'Casa':
+        oggetto['MinimiCasa'] = split[1].replace(' ', '')
+    if split[0] == 'Trasferta':
+        oggetto['MinimiTrasferta'] = split[1].replace(' ','')
+
+def CheckPrevisione(tipo, line):
+    if tipo == 'RISULTATI ESATTI':
+        CheckEsatti(line)
+        return
+    
+    if tipo == 'GOAL MINIMI':
+        CheckMin(line)
+        return
+    
+    if tipo == 'AVG CORNER':
+        CheckCorner(line)
+        return
+
+def UpdateJson():
+    if oggetto != {}:
+        jsonOggetto =   {
+                        'casa':oggetto['casa'],
+                        'trasferta':oggetto['trasferta'],
+                        'Previsione':{},
+                        'Risultati':{}
+                        }
+        if 'MinimiCasa' in oggetto:
+            jsonOggetto['Previsione']['minCasa'] = oggetto['MinimiCasa']
+        if 'MinimiTrasferta' in oggetto:
+            jsonOggetto['Previsione']['minTras'] = oggetto['MinimiTrasferta']
+        if 'Corner' in oggetto:
+            jsonOggetto['Previsione']['corner'] = oggetto['Corner']
+
+        json[today][oggetto['campionato']].append(jsonOggetto)
+    oggetto = {}
+
+
+
+today = '21-12-2022'
 with open(f'{today}.txt', 'r') as f:
     lines = f.readlines()
+    json[today] = ''
     i = 0
-    while i < len(lines):
-        line = lines[i]
-        CheckMatch(line)
-        i += 1
+    start = True
+    previsione = ''
+    for line in lines:
+        if line == '\n':
+            UpdateJson()
+            start == True
+            continue
+        if start == True:
+            CheckMatch(line)
+            start = False
+            continue
+        if start == False:
+            if 'Orario:' in line:
+                continue
+            if '(' in line and ')' in line:
+                CheckCampionato(line)
+                continue
+            if '[' in line and ']' in line:
+                previsione = PrevisioneTipo(line)
+                continue
+            CheckPrevisione(previsione, line)
+
+print(json)
